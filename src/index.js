5b2ccf8f8685a393d4dafa91640b95e9c91b873b
604karev <604karev@gmail.com>
2021-07-17 21:19:16 +0300

    feat: added solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+
+    const firstTen = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const secondTen = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const dozens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    const numberHundreds = number => Math.floor(number / 100);
+    const numberDozens = number => Math.floor(number / 10) - 2;
+    const numberUnits = number => firstTen[number % 10];
+    const hundredDozens = number => Math.floor(number % 100);
+
+    const getUnit = number => number % 10 ?
+        numberUnits(number) : '';
+
+    const getDozen = number => number < 10 ?
+        firstTen[number] : (number < 100 && number >= 20) ?
+            `${dozens[numberDozens(number)]} ${getUnit(number)}` : secondTen[number - 10];
+
+    const getReadableNumber = number => number === 0 ?
+        'zero' : number >= 100 ?
+            `${firstTen[numberHundreds(number)]} hundred ${getDozen(hundredDozens(number))}` : getDozen(number);
+
+    return getReadableNumber(number).trim();
+};
